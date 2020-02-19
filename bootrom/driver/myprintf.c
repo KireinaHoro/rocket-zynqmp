7e6196355034204fe0604a9576e844480566dd92
KireinaHoro <i@jsteward.moe>
2020-02-19 11:17:38 +0800

    implement basic hexdump

--- bootrom/driver/myprintf.c
+++ bootrom/driver/myprintf.c
@@ -4,7 +4,7 @@
 void putch(uint8_t ch) { uart_send(ch); }
 
 // void printf(char *s, ...)
-void printf(uint8_t *s, uint64_t num) {
+void printf(char *s, uint64_t num) {
   int i = 0;
   // va_list va_ptr;
   // va_start(va_ptr, s);
@@ -33,6 +33,22 @@ void printf(uint8_t *s, uint64_t num) {
         i++;
         continue;
       }
+      case 'x': {
+        // printDeci(va_arg(va_ptr,int));
+        if (num <= 0xf)
+          putch('0');
+        if (num == 0)
+          putch('0');
+        else
+          printNum(num, 16);
+        i++;
+        continue;
+      }
+      case 's': {
+        printf((char *)num, 0);
+        i++;
+        continue;
+      }
       default: {
         i++;
         continue;
@@ -48,4 +64,33 @@ void printNum(uint64_t num, int base) {
     return;
   printNum(num / base, base);
   putch("0123456789abcdef"[num % base]);
+}
+
+void hexdump(const void* data, size_t size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printf("%x ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printf(" ", 0);
+			if ((i+1) % 16 == 0) {
+				printf("|  %s \r\n", (uint64_t)ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printf(" ", 0);
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printf("   ", 0);
+				}
+				printf("|  %s \r\n", (uint64_t)ascii);
+			}
+		}
+	}
 }
--- bootrom/driver/myprintf.h
+++ bootrom/driver/myprintf.h
@@ -2,12 +2,14 @@
 #define PRINTF_HEADER
 
 #include <stdint.h>
-// #include <stdarg.h>
+#include <stddef.h>
 #include "uart.h"
 
 // extern void printf(char*, ...);
-extern void printf(uint8_t *, uint64_t);
+extern void printf(char *, uint64_t);
 void printNum(uint64_t, int);
 void putch(uint8_t);
+
+void hexdump(const void* data, size_t size);
 
 #endif
