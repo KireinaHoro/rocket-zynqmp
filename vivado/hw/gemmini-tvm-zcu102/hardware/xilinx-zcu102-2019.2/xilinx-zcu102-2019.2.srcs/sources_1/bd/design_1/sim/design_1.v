8f987c49c70a23ddae4fe61250f3da38248e889e
KireinaHoro <i@jsteward.moe>
2020-03-24 14:15:07 +0800

    add petalinux project

---
+++ vivado/hw/gemmini-tvm-zcu102/hardware/xilinx-zcu102-2019.2/xilinx-zcu102-2019.2.srcs/sources_1/bd/design_1/sim/design_1.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.2 (lin64) Build 2700185 Thu Oct 24 18:45:48 MDT 2019
+//Date        : Fri Oct 25 02:05:38 2019
+//Host        : xcosswbld06 running 64-bit Red Hat Enterprise Linux Workstation release 7.4 (Maipo)
+//Command     : generate_target design_1.bd
+//Design      : design_1
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,da_zynq_ultra_ps_e_cnt=1,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "design_1.hwdef" *) 
+module design_1
+   ();
+
+
+  design_1_zynq_ultra_ps_e_0_0 zynq_ultra_ps_e_0
+       ();
+endmodule
---
+++ vivado/hw/gemmini-tvm-zcu102/project-spec/meta-user/recipes-kernel/linux/linux-xlnx_%.bbappend
@@ -0,0 +1,5 @@
+SRC_URI += "file://bsp.cfg \
+            file://devtool-fragment.cfg \
+            "
+
+FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
