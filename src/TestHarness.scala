6bfc72588c7f2774d613ce2ec902cfa5858d1372
KireinaHoro <i@jsteward.moe>
2020-09-04 15:12:27 +0800

    SimUART C++ part can only take one UART

--- src/TestHarness.scala
+++ src/TestHarness.scala
@@ -20,7 +20,7 @@ class TestHarness(implicit p: Parameters) extends Module {
   val dut = Module(ldut.module)
 
   // Allow the debug ndreset to reset the dut, but not until the initial reset has completed
-  dut.reset := reset.asBool | dut.debug.map { debug => AsyncResetReg(debug.ndreset) }.getOrElse(false.B)
+  dut.reset := (reset.asBool | dut.debug.map { debug => AsyncResetReg(debug.ndreset) }.getOrElse(false.B)).asBool
 
   // Connect debug
   Debug.connectDebug(dut.debug, dut.resetctrl, dut.psd, clock, reset.asBool, io.success)
@@ -30,7 +30,8 @@ class TestHarness(implicit p: Parameters) extends Module {
 
   // UART - only connect the first one
   // baud rate 15M, frequency 100
-  UARTAdapter.connect(dut.uart, BigInt(15000000))
+  UARTAdapter.connect(Seq(dut.uart.head), BigInt(15000000))
+  dut.uart(1).rxd := 0.U
 }
 
 class VerilatorTop(implicit p: Parameters) extends RocketSubsystem
