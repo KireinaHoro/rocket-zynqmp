3a890176b30db1ec6411689e6ba45f35909a133a
KireinaHoro <i@jsteward.moe>
2020-09-22 20:45:57 +0800

    add newlib syscalls

---
+++ bootrom/driver/syscalls.c
@@ -0,0 +1,43 @@
+#include "uart.h"
+#include "riscv.h"
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _write(int file, char *ptr, int len) {
+    int i;
+    file = file;
+    for (i = 0; i < len; i ++) {
+        uart_send(*ptr++);
+    }
+    return len;
+}
+
+void _exit(int n) {
+    while (true) {
+        wfi();
+    }
+}
+
+extern intptr_t _heap_start;
+extern intptr_t _stack_end;
+
+intptr_t _sbrk(ptrdiff_t heap_incr) {
+    static intptr_t heap_end = (intptr_t)&_heap_start;
+
+    intptr_t prev_heap_end;
+    intptr_t new_heap_end;
+
+    prev_heap_end = heap_end;
+    new_heap_end = prev_heap_end + heap_incr;
+    
+    if (new_heap_end >= (intptr_t)&_stack_end) {
+        errno = ENOMEM;
+        return -1;
+    }
+    heap_end = new_heap_end;
+    return prev_heap_end;
+}
