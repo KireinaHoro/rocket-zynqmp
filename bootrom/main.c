7c817058e865c379b4bf0cff5f3afad764d3749a
KireinaHoro <i@jsteward.moe>
2020-09-28 17:29:14 +0800

    trap should be setup as early as possible

--- bootrom/main.c
+++ bootrom/main.c
@@ -3,12 +3,10 @@
 void main(int hartid, void *dtb) {
   if (hartid == 0) {
     uart_init();
+    // trap requires UART
+    setup_trap();
     printf(">>> Init on hart 0\n");
     printf(">>> BootROM DTB at %p\n", dtb);
-    printf(">>> Setting up machine mode trap...");
-    fflush(stdout);
-    setup_trap();
-    printf("done.\n");
 
     printf("TEST STARTUP - SPINNING\n");
 
