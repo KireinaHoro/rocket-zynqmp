ad27d44e9aa132f0444f53606aa1e5f6c00b457d
KireinaHoro <i@jsteward.moe>
2020-09-28 17:28:11 +0800

    fix missing sections

--- bootrom/driver/mem_map.h
+++ bootrom/driver/mem_map.h
@@ -2,6 +2,11 @@
 #define DDR_BASE 0x80000000
 #define DDR_SIZE 0x80000000
 #define XIP_BASE 0x71000000 // high 128Mb of configuration flash
+
+#define STACK_SIZE 0x10000
+#define STACK_TOP (DDR_BASE + DDR_SIZE)
+#define STACK_END (STACK_TOP - STACK_SIZE)
+
 // peripherals
 #define SPI_XIP_BASE 0x60000000
 #define SPI_PERIPH_BASE 0x60010000
