18c1a4133ac3f234058e17150d4635eeb5520ea8
KireinaHoro <i@jsteward.moe>
2020-10-16 15:20:38 +0800

    ADC SPI enable test

--- bootrom/main.c
+++ bootrom/main.c
@@ -1,5 +1,8 @@
 #include "bits.h"
 
+#define DEBUG_P(...) printf(__VA_ARGS__)
+//#define DEBUG_P(...)
+
 void main(int hartid, void *dtb) {
   int bitslip = 0;
   if (hartid == 0) {
@@ -12,6 +15,51 @@ void main(int hartid, void *dtb) {
     spi_init();
     printf(">>> Device startup\n");
 
+    for (int i = 7; i >= 0; --i) {
+        uint8_t readback = 0;
+
+        printf(">>> Bringing up ADC @ SC%c_%c...\n", 'A' + ((7 - i) / 2), 'A' + ((7 - i) % 2));
+
+        spi_select_slave(16 + i);
+        spi_send(0x00 | 0x00); // write 00h: reset
+        spi_send(0x80);
+        spi_deselect_slave();
+
+        // 00h reset is write-only
+
+        spi_select_slave(16 + i);
+        spi_send(0x00 | 0x03); // write 03h: pattern MSB
+        spi_send(0x80 | 0x19); // OUTTEST = 1
+        spi_deselect_slave();
+
+        spi_select_slave(16 + i);
+        spi_send(0x80 | 0x03); // read 03h: pattern MSB
+        spi_recv_multi(&readback, 1);
+        spi_deselect_slave();
+        DEBUG_P("03h = %#x\n", readback);
+
+        spi_select_slave(16 + i);
+        spi_send(0x00 | 0x04); // write 04h: pattern LSB
+        spi_send(0x84);
+        spi_deselect_slave();
+
+        spi_select_slave(16 + i);
+        spi_send(0x80 | 0x04); // read 04h: pattern LSB
+        spi_recv_multi(&readback, 1);
+        spi_deselect_slave();
+        DEBUG_P("04h = %#x\n", readback);
+
+        spi_select_slave(16 + i);
+        spi_send(0x00 | 0x02); // write 02h: output mode
+        spi_send(0x07);        // 3.5mA | no-termination | output-on | 1-lane 16bit
+        spi_deselect_slave();
+
+        spi_select_slave(16 + i);
+        spi_send(0x80 | 0x02); // read 02h: output mode
+        spi_recv_multi(&readback, 1);
+        spi_deselect_slave();
+        DEBUG_P("02h = %#x\n", readback);
+    }
 
     printf(">>> Jobs done, spinning\n");
     goto hang;
